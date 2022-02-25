85a89a6c18bc8b848cc1bf9a871013c940e9b016
Carlos Martinez <Carlos.martdelgado@gmail.com>
2022-02-25 12:56:58 +0100

    Proyect userModel

--- models/User.model.js
+++ models/User.model.js
@@ -1,24 +1,41 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      required: true,
     },
-    password: String,
-  
-    nickName: {
+    email: {
       type: String,
-    }
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    nickname: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+    },
+    comment: {
+      type: String,
+    },
+    roll: {
+      type: String,
+      ennum: ["user", "admin"],
+      default: "user",
+    },
   },
-
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
-const User = model("User", userSchema);
 
-module.exports = User;
+const UserModel = model("User", userSchema);
+
+module.exports = UserModel;
