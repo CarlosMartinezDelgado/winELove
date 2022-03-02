1546e849bbffe0943b01fcfda5a27a057dfddeac
Eduard Cabrero <eduardc.herrero@gmail.com>
2022-03-02 13:51:31 +0100

    Main

--- middleware/isLoggedAdmin.js
+++ middleware/isLoggedAdmin.js
@@ -7,14 +7,3 @@ module.exports = (req, res, next) => {
   }
 };
 
-
-
-// const isLoggedAdmin = ((req,res,next) => {
-//   if( req.session.user.role === 'admin'){
-//     next();
-//   }else{
-//     res.redirect ("/main")
-//   }
-// })
-
-// module.exports = isLoggedAdmin
